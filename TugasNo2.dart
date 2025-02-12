11af6312a55e1bf993b3dd27163f09c51353b093
IqbalHusniS <iqbalsekolah9@gmail.com>
2025-02-12 15:05:02 +0700

    Add files via upload

---
+++ TugasNo2.dart
@@ -0,0 +1,11 @@
+// nama = iqbal husni saleem
+// no absen = 15
+// kelas = XI TEL 08
+
+void main() {
+  int bintang = 5;
+
+  for (int i = 1; i <= bintang; i++) {
+    print(' ' * (bintang - i) + '*' * i);
+  }
+}
